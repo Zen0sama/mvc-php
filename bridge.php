87b1cff65ddee0ce66221e12ebc8be37163e4c7f
Zen0sama <wibusama1998@gmail.com>
2022-07-15 14:57:47 +0700

    first commit

---
+++ bridge.php
@@ -0,0 +1,4 @@
+<?php
+// Process URL from browser
+require_once "./core/app.php";
+?>
---
+++ controllers/home.php
@@ -0,0 +1,13 @@
+<?php
+class home
+{
+    /* default */
+    function index()
+    {
+        echo 'home controller';
+    }
+
+    function show($a, $b)
+    {
+    }
+}
